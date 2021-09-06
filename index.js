c80668057fceb671e4d216befa5af364ad5122ea
James Hoofard <james.hoofard.iii@gmail.com>
2021-09-06 16:35:26 -0500

    made my inquirer prompt, asks correct questions when prompted

--- index.js
+++ index.js
@@ -20,11 +20,95 @@ const questions = [
 
     },
     {
-        message: 'What is the name of this Employee?',
+        message: 'What is the first name of this Employee?',
         type: 'input',
-        name: 'employeeName',
+        name: 'employeeFirstName',
         when: (response) => 
-        response.actionPrompt === 'Add Employee'
+        response.actionPrompt === 'Add Employees'
+    },
+    {
+        message: 'What is the last name of this Employee?',
+        type: 'input',
+        name: 'employeeLastName',
+        when: (response) => 
+        response.actionPrompt === 'Add Employees'
+    },
+    {
+        message: 'What is the Role of this Employee?',
+        type: 'list',
+        choices: [
+            'Manager',
+            'Sales',
+            'Marketing',
+            'Engineer',
+            'Customer Service'
+        ],
+        name: 'EmployeeRole',
+        when: (response) => 
+        response.actionPrompt === 'Add Employees'
+    },
+    {
+        message: "Who is this Employee's Manager?",
+        type: 'list',
+        choices: [
+            'Sally Davis',
+            'Robert Smith',
+            'Kennedy Bonnet',
+            'Simon Peterson',
+            'no Manager'
+        ],
+        name: 'employeeManager',
+        when: (response) => 
+        response.actionPrompt === 'Add Employees'
+    },
+    {
+        message: 'What is the name of this Department?',
+        type: 'input',
+        name: 'departmentName',
+        when: (response) => 
+        response.actionPrompt === 'Add Department'
+        
+    },
+    {
+        message: 'What is the name of this Role?',
+        type: 'input',
+        name: 'roleName',
+        when: (response) => 
+        response.actionPrompt === 'Add Role'
+    },
+    {
+        message: 'What is the Salary of this Role?',
+        type: 'number',
+        name: 'roleSalary',
+        when: (response) => 
+        response.actionPrompt === 'Add Role'
     }
 
 ]
+
+function init() {
+    
+    inquirer.prompt(questions)
+    .then((response) => {
+
+        
+    if (response.actionPrompt === 'Quit'){
+       process.exit
+      
+
+        
+
+    }
+    else {
+        init()
+    }
+
+
+
+
+
+})
+}
+
+
+init()
