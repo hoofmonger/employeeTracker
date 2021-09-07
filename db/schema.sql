572516023a4dc3aad75d7142ccc8d6dcafac718a
James Hoofard <james.hoofard.iii@gmail.com>
2021-09-07 13:51:10 -0500

    fixed some sql bugs

--- db/schema.sql
+++ db/schema.sql
@@ -5,24 +5,24 @@ USE employees;
 
 DROP TABLE IF EXISTS departments;
 CREATE TABLE departments (
-    id INT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30)
 );
 
 DROP TABLE IF EXISTS roles;
 CREATE TABLE roles (
-  id INT NOT NULL,
-  role_name VARCHAR(30)
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  role_name VARCHAR(30),
   salary DECIMAL,
   department_id INT 
 );
 
 DROP TABLE IF EXISTS employee;
 CREATE TABLE employee (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL
+  role_id INT NOT NULL,
   manager_id INT
 );
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -3,22 +3,22 @@ VALUES(001, 'Manager'),
 (002, 'Sales'), 
 (003, 'Marketing'), 
 (004, 'Engineering'), 
-(005, 'Customer Service')
+(005, 'Customer Service');
 
 INSERT INTO roles (id, role_name, salary, department_id)
 VALUES(001, 'CEO', 1000000, 001), 
 (002, 'Sales and Marketing Director', 200000, 001), 
-(003, 'Director of Research and Development', 200000, 001), 
+(003, 'Director of Research', 200000, 001), 
 (004, 'Customer Service Director', 200000, 001), 
 (005, "Senior Sales Rep", 150000, 002), 
 (006, "Sales Rep", 85000, 002), 
 (007, "Marketing Specialist", 135000, 003), 
 (008, "Marketing Intern", 40000, 003), 
-(009, "Chief Engineer" 300000, 004), 
+(009, "Chief Engineer", 300000, 004), 
 (010, "Engineer", 180000, 004), 
 (011, "Engineering Intern", 40000, 004), 
 (012, "Customer Service - Shift Lead", 115000, 005), 
-(013, "Customer Service Representive", 85000, 005)
+(013, "Customer Service Representive", 85000, 005);
 
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
@@ -33,9 +33,9 @@ VALUES(001, 'Sally', 'Davis', 001, 001),
 (009, 'Halbrick', 'Caneswick', 006, 002),
 (010, 'Tiberius', 'Cornswallup', 006, 002),
 (011, 'Jimmothy', 'Bonjourno', 007, 002),
-(012, 'Melisandra', 'Money-Penny', 008, 002)
+(012, 'Melisandra', 'Money-Penny', 008, 002),
 (013, 'Julius', 'Axelfort', 008, 002),
-(014, 'Waltarius' 'Who', 009, 003),
+(014, 'Waltarius', 'Who', 009, 003),
 (015, 'Bobert', 'Carlsburg', 010, 003),
 (016, 'Jonly', 'Bonly', 010, 003),
 (017, 'Charlston', 'Custardson', 011, 003),
@@ -46,4 +46,4 @@ VALUES(001, 'Sally', 'Davis', 001, 001),
 (022, 'Airwrecka', 'Moldovo', 013, 004),
 (023, 'Veronica', 'McSleezy', 013, 004),
 (024, 'Vlad', 'Bobniski', 013, 004),
-(025, 'Majorie', 'Horsepaste', 013, 004)
+(025, 'Majorie', 'Horsepaste', 013, 004);
